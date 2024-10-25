8ab526aa122136fec9111c7801fb6e0a90d5a2eb
seyaobey-dev <seyaobey.dev@gmail.com>
2024-10-25 01:40:27 +0100

    fix: fix e2e test

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,17 +1,17 @@
 import axios from "axios";
-import { fieldsMapping, mockData } from "./constants";
+import { fieldsMapping } from "./constants";
 
 import { QueryBuilderDataProvider } from "./features/query-builder/_providers/query-builder-data-provider";
 import { QueryBuilderTree } from "./features/query-builder/query-builder-tree";
 import { useState } from "react";
 import { Combinator, GroupQuery } from "../types";
-import { rebuildJson } from "./features/query-builder/utils";
+import { transformArrayOfGroupsToJSON } from "./features/query-builder/utils";
 
 function App() {
   const [result, setResult] = useState<Combinator | null>();
 
-  const handleSubmit = async (data: GroupQuery[]) => {
-    const combinator: Combinator = rebuildJson(data);
+  const handleSubmit = async (arrayOfGroups: GroupQuery[]) => {
+    const combinator: Combinator = transformArrayOfGroupsToJSON(arrayOfGroups);
 
     try {
       await axios.post("/api/save-rules", combinator);
@@ -24,13 +24,14 @@ function App() {
 
   return (
     <div className="flex flex-col items-start">
-      <QueryBuilderDataProvider root={mockData} fieldsMapping={fieldsMapping}>
+      <QueryBuilderDataProvider fieldsMapping={fieldsMapping}>
 
         <QueryBuilderTree onSubmit={handleSubmit} onCancel={() => {
-          // TODO: handle cancel
           setResult(null);
         }} />
+        
       </QueryBuilderDataProvider>
+
       <div>
         <p className="mt-5 pl-4">{result ? JSON.stringify(result, null, 2) : null}</p>
       </div>
